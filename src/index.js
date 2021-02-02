8d11d670beb781c593612232f5aab44629789420
SaharSuleman <qazirameezahmed569@gmail.com>
2021-02-02 15:35:42 +0500

    change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import reactDOM from "react-dom";
-import css from  "./index.css" 
+import css from "./index.css"
+
+
+
 
 function MyApp() {
 
@@ -19,17 +22,15 @@ function MyApp() {
     e.preventDefault()
 
     var todoText = document.getElementById('TodoText').value
-    if(todoText === ""){
-      alert("some text ")
+    if (todoText === "") {
+      alert("hello i m react")
     }
 
     setData((previouseVlaue) => {
       return previouseVlaue.concat(todoText)
-      
+
     })
-    // var text = document.createElement('li')
-    // text.innerHTML = todoText
-    // document.getElementById('ListPost').innerHTML = text
+
   }
 
   const remove = (index) => {
@@ -40,22 +41,21 @@ function MyApp() {
 
 
 
-
-
   return (
-    <div  className="todoList">
+    <div className="todoList">
       <form onSubmit={ToDoApp} className="form">
-      <h1>ToDo App</h1>
-        <input type="text" id="TodoText" placeholder="Type......." /><button > Add </button>
+        <h1>ToDo App</h1>
+        <input type="text" id="TodoText" placeholder="Type......." />
+        <button className="butt">ADD LIST</button>
       </form>
 
       {Data.map((value, index) => {
 
-        
+
         return (
           <div className="flex" key={index} >
-            <p className="listText">{value}</p><i className="fas fa-trash-alt button" onClick={()=> remove(index)}></i>
-            
+            <p className="listText">{value}</p><i className="fas fa-trash-alt button" onClick={() => remove(index)}></i>
+
           </div>
         )
       })}
